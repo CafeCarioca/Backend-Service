f58ff3695f34575e8d79d66a3abb6f8e9ff74c99
Enz1n <enzodeleonb49@gmail.com>
2025-03-28 11:17:07 -0300

    add validatetoke and token

--- routes/products.js
+++ routes/products.js
@@ -2,16 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-
-// Define a route
-
-router.get('/101', (req, res) => {
-    res.send('this is product 101 route');// this gets executed when user visit http://localhost:3000/product/101
-});
-
-router.get('/102', (req, res) => {
-    res.send('this is product 102 route');// this gets executed when user visit http://localhost:3000/product/102
-});
+const validateToken = require('../middlewares/authMiddleware');
 
 
 // Endpoints productos
@@ -20,11 +11,11 @@ router.get('/', productController.getAllProducts);
 
 router.get('/:id', productController.getProductById);
 
-router.post('/', productController.createProduct);
+router.post('/',validateToken, productController.createProduct);
 
-router.put('/:id', productController.updateProduct);
+router.put('/:id',validateToken, productController.updateProduct);
 
-router.delete('/:id', productController.deleteProduct);
+router.delete('/:id',validateToken, productController.deleteProduct);
 
 
 
